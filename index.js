8939070cc6513c4a87936454fda474ed856343ff
meenatchi <meenatchicse44@gmail.com>
2023-11-28 07:06:06 +0530

    update,delete fs

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function calculate(num1,num2,operation){
 }
 //calculate(parseInt(n1),parseInt(n2),op)
 
-import { readFile, writeFile } from "fs";//file systm access
+const fs =require("fs");//file systm access
 
 // reading a file
-readFile("sample.txt", "utf-8", (err, data) => {
+fs.readFile("sample.txt", "utf-8", (err, data) => {
     if (err) {
       console.log("Error Occured :", err);
     } else {
@@ -28,7 +28,7 @@ readFile("sample.txt", "utf-8", (err, data) => {
   });
 //write
   const content = "This text file is written in nodejs";
-  writeFile("./node.txt", content, (err) => {
+  fs.writeFile("./node.txt", content, (err) => {
     if (err) {
       console.log("Error Occured :", err);
     } else {
@@ -36,32 +36,32 @@ readFile("sample.txt", "utf-8", (err, data) => {
     }
   });
   
-//   const appendContent = `\n New line added in a file`;
-//   fs.appendFile("./node.txt", appendContent, (err) => {
-//     if (err) {
-//       console.log("error", err);
-//     } else {
-//       console.log("file updated successfully");
-//     }
-//   });
+   const appendContent = `\n New line added in a file`;
+  fs.appendFile("./node.txt", appendContent, (err) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      console.log("file updated successfully");
+    }
+  });
   
   // delete a file
-//   fs.unlink("./sample.txt", (err) => {
-//     if (err) {
-//       console.log("error", err);
-//     } else {
-//       console.log("file deleted successfully");
-//     }
-//   });
+  fs.unlink("./sample.txt", (err) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      console.log("file deleted successfully");
+    }
+  });
 
   // read the config files
- readFile(configPath,"utf-8",(err, data) => {
-    if (err) {
-      console.log("Error Occured :", err);
-    } else {
-      console.log(data);
-      const config=JSON.parse(data);
-      console.log(config);
-    }
-});
+//  readFile(configPath,"utf-8",(err, data) => {
+//     if (err) {
+//       console.log("Error Occured :", err);
+//     } else {
+//       console.log(data);
+//       const config=JSON.parse(data);
+//       console.log(config);
+//     }
+// });
    
