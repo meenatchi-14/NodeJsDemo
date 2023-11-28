6e8cef1eeadb8b5e468aae78374af40914b3099c
meenatchi <meenatchicse44@gmail.com>
2023-11-28 19:40:41 +0530

    postman demo

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 import express from "express"
 const app=express();
-const resturants=[
+//middle wares
+app.use(express.json());
+let resturants=[
     {
+        id:"1",
         name:"KFC",
         special:"crispy chicken"
     },{
+        id:"2",
         name:"A2B",
         special:" chicken" 
-    }
-]
+    },{
+        id:"3",
+        name:"Chicken",
+        special:"65"
+    }]
 //get info
-app.get("/home",(req,res)=>{
+app.get("/res",(req,res)=>{
     res.send(resturants);
 });
+//
+app.post("/add/res",(req,res)=>{
+  
+    let newResturants={
+...req.body,
+id:(resturants.length + 1).toString(),
+    };
+    resturants=[...resturants,newResturants]
+    res.send(resturants)
+})
+app.put("/edit/res/:id", (req, res) => {
+    
+    const { id } = req.params;
+     const findRes = resturants.find((res) => res.id === id);
+     findRes.special = req.body.special;
+     res.send(resturants);
+  });
+  
+  app.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    const newset = resturants.filter((res) => res.id != id);
+    resturants = [...newset];
+    res.send(`Deleted Resturant`);
+  });
 //listen to the server
-app.listen(9000,()=>console.log("server started in local server:9000 "))
+app.listen(9000,()=>console.log("local server started 9000"))
