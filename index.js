277093f0500ca5b2983b50e00a3cee2200e84690
meenatchi <meenatchicse44@gmail.com>
2023-11-28 08:56:05 +0530

    sample in port

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express"
 const app=express();
+const resturants=[
+    {
+        name:"KFC",
+        special:"crispy chicken"
+    },{
+        name:"A2B",
+        special:" chicken" 
+    }
+]
 //get info
 app.get("/home",(req,res)=>{
-    res.send({name:"new Application"});
+    res.send(resturants);
 });
 //listen to the server
 app.listen(9000,()=>console.log("server started in local server:9000 "))
